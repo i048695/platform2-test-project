8ba7fe8a2c0d47ff06830eba95f8c416b1ce9ab6
i048695 <lev.zak@sap.com>
2019-06-16 16:07:19 +0300

    Update App.controller.js

--- App.controller.js
+++ App.controller.js
@@ -1,8 +1,7 @@
-var sap = {ui: {define: function() {}}};
 sap.ui.define(["sap/m/Button", "sap/ui/model/json/JSONModel"],
 
 
-    function (oButton, oModel) {
-        // your code
+function (oButton, oModel) {
+	// your code
 
-    });
+});
